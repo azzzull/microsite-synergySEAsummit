3fe1770e72ed5fe48c93826699189e40e97ca15a
Azzzull <azulkarnaen7@gmail.com>
2025-09-28 10:45:27 +0700

    benerin ukuran maps pada mobile view

--- src/app/location/page.tsx
+++ src/app/location/page.tsx
@@ -39,7 +39,7 @@ export default function LocationPage() {
           {/* Map and Photos Section */}
           <div className="mb-12 grid grid-cols-1 lg:grid-cols-3 gap-4">
             {/* Map */}
-            <div className="lg:col-span-2 h-[30px] sm:h-[420px]">
+            <div className="lg:col-span-2 h-[420px] sm:h-[420px]">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3943.791309749572!2d115.16520537472309!3d-8.71135769133782!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd246c1b2997c97%3A0x7e36cd3ef6793772!2sThe%20Stones%20Hotel%20Legian!5e0!3m2!1sid!2sid!4v1757000867033!5m2!1sid!2sid"
                 width="100%"
